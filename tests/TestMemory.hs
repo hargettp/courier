06cb71915d0916044a4ad4660984f675ab4a99c2
Phil Hargett <phil@haphazardhouse.net>
2014-06-29 13:20:30 +0000

    Fixes for #5. Ensured that transports are shutdown in more tests.

--- tests/TestMemory.hs
+++ tests/TestMemory.hs
@@ -7,6 +7,8 @@ import Network.Transport.Memory
 
 -- external imports
 
+import Control.Exception
+
 import Data.Serialize
 
 import Test.Framework
@@ -24,52 +26,58 @@ tests = [
     testCase "mem-unbind" testEndpointBindUnbind,
     testCase "mem-sendReceive" testEndpointSendReceive,
     testCase "mem-transport" testMemoryTransport
-  ] 
-  
-testEndpointTransport :: Assertion  
-testEndpointTransport = do  
+  ]
+
+testEndpointTransport :: Assertion
+testEndpointTransport = do
   transport <- newMemoryTransport
   _ <- newEndpoint [transport]
   return ()
-  
+
 testEndpointBind :: Assertion
-testEndpointBind = do  
+testEndpointBind = do
   let name1 = "endpoint1"
   transport <- newMemoryTransport
-  endpoint <- newEndpoint [transport]
-  Right () <- bindEndpoint endpoint name1
-  return ()
+  finally (do
+          endpoint <- newEndpoint [transport]
+          Right () <- bindEndpoint endpoint name1
+          return ())
+      (shutdown transport)
 
 testEndpointBindUnbind :: Assertion
-testEndpointBindUnbind = do  
+testEndpointBindUnbind = do
   let name1 = "endpoint1"
   transport <- newMemoryTransport
-  endpoint <- newEndpoint [transport]
-  Right () <- bindEndpoint endpoint name1
-  unbound <- unbindEndpoint endpoint name1
-  case unbound of
-    Left err -> assertFailure $ "Unbind failed: " ++ err
-    Right () -> assertBool "Unbind succeeded" True
-  return ()
-  
-testEndpointSendReceive :: Assertion  
+  finally (do
+          endpoint <- newEndpoint [transport]
+          Right () <- bindEndpoint endpoint name1
+          unbound <- unbindEndpoint endpoint name1
+          case unbound of
+            Left err -> assertFailure $ "Unbind failed: " ++ err
+            Right () -> assertBool "Unbind succeeded" True
+          return ())
+      (shutdown transport)
+
+testEndpointSendReceive :: Assertion
 testEndpointSendReceive = do
   let name1 = "endpoint1"
       name2 = "endpoint2"
   transport <- newMemoryTransport
-  endpoint1 <- newEndpoint [transport]
-  endpoint2 <- newEndpoint [transport]
-  Right () <- bindEndpoint endpoint1 name1
-  Right () <- bindEndpoint endpoint2 name2
-  _ <- sendMessage endpoint1 name2 $ encode "hello!"
-  msg <- receiveMessage endpoint2    
-  assertEqual "Received message not same as sent" (Right "hello!") (decode msg)
-  return ()
-  
+  finally (do
+          endpoint1 <- newEndpoint [transport]
+          endpoint2 <- newEndpoint [transport]
+          Right () <- bindEndpoint endpoint1 name1
+          Right () <- bindEndpoint endpoint2 name2
+          _ <- sendMessage endpoint1 name2 $ encode "hello!"
+          msg <- receiveMessage endpoint2
+          assertEqual "Received message not same as sent" (Right "hello!") (decode msg)
+          return ())
+      (shutdown transport)
+
 -- Memory tests
-  
+
 testMemoryTransport :: Assertion
 testMemoryTransport = do
-  _ <- newMemoryTransport
-  return ()
+    transport <- newMemoryTransport
+    shutdown transport
 
